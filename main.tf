7b89cda66f5974b45f4e9618d8b61f14ceedd6f4
Manoj Kumar <napestershine@gmail.com>
2022-07-31 22:48:37 +0200

    cluster of web server with tf

--- main.tf
+++ main.tf
@@ -11,44 +11,158 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-052efd3df9dad4825"
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
+  # Required wen using a launch configuration with an auto scaling group
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  user_data_replace_on_change = true
 
-  tags = {
-    "Name" = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  max_size = 10
+  min_size = 2
+
+  tag {
+    key                 = "Name"
+    propagate_at_launch = true
+    value               = "terraform-asg-example"
   }
 }
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-  ingress = [{
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = "aws security group for ec2"
-    prefix_list_ids  = []
-    security_groups  = []
-    self             = false
-    from_port        = 8080
-    protocol         = "tcp"
-    to_port          = 8080
-    ipv6_cidr_blocks = ["::/0"]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "aws security group for ec2"
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      from_port        = var.server_port
+      protocol         = "tcp"
+      to_port          = var.server_port
+      ipv6_cidr_blocks = ["::/0"]
 
-  }]
+    }
+  ]
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_alb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
 
 
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP request
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
+variable "server_port" {
+  description = "The port that http server will use"
+  type        = number
+  default     = 8080
+}
 
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
